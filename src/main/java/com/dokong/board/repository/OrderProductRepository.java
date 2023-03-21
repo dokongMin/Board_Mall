f6373dfb4dba261da6d3528de9c77529f42b4b1f
minhwan <jeff970069@gmail.com>
2023-03-21 16:28:25 +0900

    #7 #8 [feature] : OrderProduct 서비스 & 테스트 코드 작성 완료
    OrderProduct 서비스 코드 작성 완료.
    OrderProduct 테스트 코드 작성 완료.

---
+++ src/main/java/com/dokong/board/repository/OrderProductRepository.java
@@ -0,0 +1,11 @@
+package com.dokong.board.repository;
+
+import com.dokong.board.domain.OrderProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+
+    Optional<OrderProduct> findById(Long id);
+}
---
+++ src/test/java/com/dokong/board/web/service/OrderProductServiceTest.java
@@ -0,0 +1,99 @@
+package com.dokong.board.web.service;
+
+import com.dokong.board.domain.OrderProduct;
+import com.dokong.board.domain.Product;
+import com.dokong.board.repository.OrderProductRepository;
+import com.dokong.board.web.dto.CategoryDto;
+import com.dokong.board.web.dto.SaveOrderProductDto;
+import com.dokong.board.web.dto.product.SaveProductDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+class OrderProductServiceTest {
+
+
+    @Autowired
+    private OrderProductService orderProductService;
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Test
+    @DisplayName("주문_상품_저장")
+    public void saveOrderProduct () throws Exception{
+        // given
+        CategoryDto getCategoryDto = getCategoryDto();
+        CategoryDto categoryDto = categoryService.saveCategory(getCategoryDto);
+
+        SaveProductDto getSaveProductDto = getSaveProductDto();
+        SaveProductDto saveProductDto = productService.saveProduct(getSaveProductDto, categoryDto.getCategoryName());
+        Product product = productService.findById(saveProductDto.getId());
+
+        SaveOrderProductDto saveOrderProductDto = getSaveOrderProductDto();
+        SaveOrderProductDto saveOrderProductDtoEntity = orderProductService.saveOrderProduct(saveOrderProductDto, product.getId());
+        OrderProduct orderProduct = orderProductService.findById(saveOrderProductDtoEntity.getId());
+
+        // then
+        assertThat(orderProduct.getOrderItemPrice()).isEqualTo(10000);
+        assertThat(orderProduct.getOrderItemCount()).isEqualTo(10);
+        assertThat(product.getItemStock()).isEqualTo(90);
+     }
+
+     @Test
+     @DisplayName("주문_상품_취소")
+     public void cancelOrderProduct () throws Exception{
+         CategoryDto getCategoryDto = getCategoryDto();
+         CategoryDto categoryDto = categoryService.saveCategory(getCategoryDto);
+
+         SaveProductDto getSaveProductDto = getSaveProductDto();
+         SaveProductDto saveProductDto = productService.saveProduct(getSaveProductDto, categoryDto.getCategoryName());
+         Product product = productService.findById(saveProductDto.getId());
+
+         SaveOrderProductDto saveOrderProductDto = getSaveOrderProductDto();
+         SaveOrderProductDto saveOrderProductDtoEntity = orderProductService.saveOrderProduct(saveOrderProductDto, product.getId());
+         OrderProduct orderProduct = orderProductService.findById(saveOrderProductDtoEntity.getId());
+         //when
+         orderProductService.cancelOrderProduct(orderProduct.getId());
+         // then
+         assertThat(product.getItemStock()).isEqualTo(100);
+         assertThat(product.getOrderProducts().size()).isEqualTo(0);
+         assertThat(orderProductRepository.findAll().size()).isEqualTo(0);
+      }
+
+    private SaveOrderProductDto getSaveOrderProductDto() {
+        return SaveOrderProductDto.builder()
+                .orderItemPrice(10000)
+                .orderItemCount(10)
+                .build();
+    }
+
+    private CategoryDto getCategoryDto() {
+        return CategoryDto.builder()
+                .categoryName("과일")
+                .build();
+    }
+
+    private SaveProductDto getSaveProductDto() {
+        return SaveProductDto.builder()
+                .itemName("사과")
+                .itemPrice(10000)
+                .itemStock(100)
+                .build();
+    }
+}
